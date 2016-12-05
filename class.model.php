9b13bb3749805cce133dd7aedf5e4d06b2e2ba06
Harry Finn <harry@harryfinn.co.uk>
2016-12-05 23:56:58 +0000

    Add Taxonomy register & label generators for CPTs & Taxomonies

--- class.model.php
+++ class.model.php
@@ -25,6 +25,8 @@ class Model {
   public function __construct($post_type, array $cpt_args = []) {
     $this->post_type = $post_type;
 
+    if(post_type_exists($this->post_type)) return;
+
     $this->register_custom_post_type(
       $post_type,
       wp_parse_args($cpt_args, $this->default_cpt_args)
@@ -55,6 +57,16 @@ class Model {
     register_post_type($cpt, $cpt_args);
   }
 
+  private function register_custom_taxonomies($taxonomies) {
+    foreach($taxonomies as $taxonomy => $taxonomy_options) {
+      register_taxonomy(
+        $taxonomy,
+        $taxonomy_options['post_type'],
+        $taxonomy_options['taxonomy_args']
+      );
+    }
+  }
+
   private function set_paged_value(array $query_args) {
     $default_paged = !empty($query_args['paged']) ?
       $query_args['paged'] :
@@ -64,5 +76,41 @@ class Model {
     $query_args['paged'] = $paged_query_var ? $paged_query_var : $default_paged;
 
     return $query_args;
+  }
+
+  private function generate_cpt_labels_for($singular, $pural) {
+    return [
+      'name' => $pural,
+      'singular_name' => $singular,
+      'add_new' => 'Add New',
+      'add_new_item' => "Add New $singular",
+      'edit_item' => "Edit $singular",
+      'new_item' => "New $singular",
+      'view_item' => "View $singular",
+      'search_items' => "Search $pural",
+      'not_found' => "No $pural found",
+      'not_found_in_trash' => "No $pural found in Trash",
+      'parent_item_colon' => "Parent $singular",
+      'all_items' => "All $pural",
+      'archives' => "$singular Archives",
+      'insert_into_item' => "Insert into $singular",
+      'uploaded_to_this_item' => "Uploaded to this $singular",
+      'not_found' => "No $pural found"
+    ];
+  }
+
+  private function generate_taxonomy_labels_for($singular, $pural) {
+    return [
+      'name' => $pural,
+      'singular_name' => $singular,
+      'all_items' => "All $pural",
+      'parent_item' => "Parent $singular",
+      'parent_item_colon' => "Parent $singular:",
+      'edit_item' => "Edit $singular",
+      'update_item' => "Update $singular",
+      'add_new_item' => "Add New $singular",
+      'new_item_name' => "New $singular Name",
+      'menu_name' => $pural
+    ];
   }
 }
